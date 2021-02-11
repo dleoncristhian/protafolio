97e14b6b07687bd81da7920c756c0ddcbd3136de
Cristhian <dleoncristhian@gmail.com>
2021-02-11 12:42:54 -0600

    appbar y header

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,27 +1,132 @@
 import React from 'react';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import InputBase from '@material-ui/core/InputBase';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
-function Navegador (){
-    return (
-        <nav className="navbar navbar-expand-lg site-navbar navbar-light bg-light" id="pb-navbar">
+const useStyles = makeStyles((theme) => ({
+	root: {
+		backgroundColor : '#00a196'
+	},
+	tool: {
+		backgroundColor: '#00a196'
+	},
+	grow: {
+	  flexGrow: 1,
+	},
+	menuButton: {
+	  marginRight: theme.spacing(2),
+	  backgroundColor : '#00a196'
+	},
+	texto : {
+		fontFamily :'Goudy Old Style',
+		position : 'right'
+	},
+	title: {
+	  display: 'none',
+	  [theme.breakpoints.up('sm')]: {
+		display: 'block',
+	  },
+	  fontFamily :'Goudy Old Style'
+	},
+	search: {
+		position: 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		'&:hover': {
+		  backgroundColor: fade(theme.palette.common.white, 0.25),
+		},
+		marginRight: theme.spacing(2),
+		marginLeft: 0,
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+		  marginLeft: theme.spacing(3),
+		  width: 'auto',
+		},
+	  },
+	  searchIcon: {
+		padding: theme.spacing(0, 2),
+		height: '100%',
+		position: 'absolute',
+		pointerEvents: 'none',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	  },
+	  inputRoot: {
+		color: 'inherit',
+		fontFamily :'Goudy Old Style',
+	  },
+	  inputInput: {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('md')]: {
+		  width: '20ch',
+		},
+	  }
+	}));
 
-		<div className="container">
-			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon"></span>
-			</button>
+export default function Header() {
+  const classes = useStyles();
+
+  const [anchorE1, setAnchorE1] = React.useState(null);
+
+  const handleClick = (event) => {
+	  setAnchorE1(event.currentTarget);
+  };
+
+  const handleClose = () => {
+	  setAnchorE1(null);
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" >
+        <Toolbar className={classes.tool}>
+		  <IconButton edge="start" className={classes.menuButton} 
+		   color="inherit" aria-label="menu" onClick={handleClick} >
+            <MenuIcon />
+          </IconButton>
+		  <Menu
+        id="simple-menu"
+        anchorEl={anchorE1}
+        keepMounted
+        open={Boolean(anchorE1)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+          <Typography variant="h5" className={classes.title}>
+            THEOS
+          </Typography>
 
 
-			<div className="collapse navbar-collapse justify-content-md-center" id="navbarsExample09">
-				<ul className="navbar-nav">
-					<li className="nav-item"><a className="nav-link" href="#section-home">Home</a></li>
-					<li className="nav-item"><a className="nav-link" href="#section-portfolio">Portfolio</a></li>
-					<li className="nav-item"><a className="nav-link" href="#section-resume">Resume</a></li>
-					<li className="nav-item"><a className="nav-link" href="#section-about">About</a></li>
-					<li className="nav-item"><a className="nav-link" href="#section-contact">Contact</a></li>
-				</ul>
+          <div className={classes.search}>
+			<div className={classes.searchIcon}>
+				<SearchIcon/>
 			</div>
-		</div>
-	</nav>
-    )
+			<InputBase
+			 placeholder="Buscar..."
+			 classes={{
+				 root: classes.inputRoot,
+				 input: classes.inputInput,
+			 }}
+			/>
+		  </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-
-export default Navegador;
--- src/componentes/Presentacion.js
+++ src/componentes/Presentacion.js
@@ -1,21 +1,64 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		maxWidth : 'none',
+		position : 'relative',
+		margin : 'auto',
+		fontFamily :'Goudy Old Style'
+
+	},
+	carousel: {
+		display : 'none'
+	},
+	slides: {
+		width: "100%"
+	},
+	numSlide: {
+		color: "#f2f2f2",
+		fontSize: "12px",
+		padding: "8px 12px",
+		position: "absolute",
+		top: "0"
+	},
+	text0: {
+		color: '#000000',
+		fontSize: '20px:',
+		position: 'absolute',
+		padding: '8px 12px',
+		bottom: '8px',
+		width: '100px',
+		textAlign: 'center'
+		
+	},
+	text: {
+		display: 'none',
+		textAlign: 'center',
+		color: '#FFFFFF',
+		padding: '8px 12px',
+		bottom: '8px',
+		[theme.breakpoints.up('sm')]: {
+		  display: 'block',
+		},
+		fontFamily :'Goudy Old Style'
+	}
+
+}))
 
 function Presentacion(){
+	const classes = useStyles();
+
     return(
-        <section className="site-hero" 
-        style={{backgroundImage: 'url("images/image_1.jpg");' }} 
-        id="section-home" data-stellar-background-ratio="0.5">
-		<div className="container">
-			<div className="row intro-text align-items-center justify-content-center">
-				<div className="col-md-10 text-center pt-5">
-
-					<h1 className="site-heading site-animate">Hola, me llamo<strong class="d-block">Cristhian de Leon</strong></h1>
-					<strong className="d-block text-white text-uppercase letter-spacing">y esta es mi pagina</strong>
-
-				</div>
-			</div>
+		<div className={classes.root}>
+			<div className={classes.carousel}></div>
+			<img className={classes.slides} src="images/top1.jpg" alt="" />
+			<Typography variant="subtittle1" className={classes.text}>
+            	Bienvenidos
+          	</Typography>
 		</div>
-	</section>
     )
 }
 
